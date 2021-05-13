aea2c3845f085ccef62a597bfdcf051ab5accf2d
kel pande <fmcmacaspac@protonmail.com>
2021-05-13 21:27:51 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      clr900: "#15202B", // Background Color
+      clr800: "#192734", // Trends Color
+      clr700: "#1C2732", // Hover Color (Cards)
+      clr600: "#162D40", // Hover Color (Buttons)
+      clr500: "#19608E", // Disabled Button (Color)
+      clr400: "#1B83C5", // Small Icons Color (InputBox)
+      clr300: "#1DA1F2", // Active Home Color
+      clr200: "#1A91DA", // Tweet button color
+      clr100: "#FFFFFF", // White Color
+      linesClr: "#38444D", // border lines
+      grayClr: "#8899A6", // username color
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
